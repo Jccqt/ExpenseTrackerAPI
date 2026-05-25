ac96362597f54b324ba1ad6041252acaf76450e1
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-05-25 10:05:35 +0800

    Add project files.

---
+++ ExpenseTrackerAPI/Context/ExpenseDb.cs
@@ -0,0 +1,11 @@
+﻿using ExpenseTrackerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerAPI.Context
+{
+    public class ExpenseDb : DbContext
+    {
+        public ExpenseDb(DbContextOptions<ExpenseDb> options) : base(options) { }
+        public DbSet<Expense> Expenses => Set<Expense>();
+    }
+}
---
+++ ExpenseTrackerAPI/Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTrackerAPI.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime DateLogged { get; set; } = DateTime.Now;
+    }
+}
