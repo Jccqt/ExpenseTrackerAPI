4fcfd896c00901b9faa0d09cfecbbbc7eec18933
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-05-27 15:17:36 +0800

    feat(expenses): add ExpenseDTO and IExpenseRepository

---
+++ ExpenseTrackerAPI/DTOs/ExpenseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTrackerAPI.DTOs
+{
+    public class ExpenseDTO
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime DateLogged { get; set; }
+    }
+}
---
+++ ExpenseTrackerAPI/Interface/IExpenseRepository.cs
@@ -0,0 +1,10 @@
+﻿using ExpenseTrackerAPI.DTOs;
+
+namespace ExpenseTrackerAPI.Interface
+{
+    public interface IExpenseRepository
+    {
+        Task<List<ExpenseDTO>> GetAllExpenses();
+        Task<ExpenseDTO> GetExpenseById(int expenseId);
+    }
+}
