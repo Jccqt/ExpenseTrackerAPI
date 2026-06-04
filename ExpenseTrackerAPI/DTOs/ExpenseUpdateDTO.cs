89e177773e8f49ed83c1fdf33e6ba3d72e23cd44
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-04 09:20:24 +0800

    feat(expenses): add ExpenseUpdateDTO

---
+++ ExpenseTrackerAPI/DTOs/ExpenseUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpenseTrackerAPI.DTOs
+{
+    public class ExpenseUpdateDTO
+    {
+        public string? Description { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
