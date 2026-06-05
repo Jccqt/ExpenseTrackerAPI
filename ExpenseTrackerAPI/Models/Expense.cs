56f3977769bba41bd8190e0985b1574f952af382
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-06 07:57:18 +0800

    feat(expenses): add Status attribute in Expense.cs
    Status will have a default value of 1, which will be considered as 'active'.

--- ExpenseTrackerAPI/Models/Expense.cs
+++ ExpenseTrackerAPI/Models/Expense.cs
@@ -6,5 +6,6 @@
         public string Description { get; set; } = string.Empty;
         public decimal Amount { get; set; }
         public DateTime DateLogged { get; set; } = DateTime.Now;
+        public int Status { get; set; } = 1;
     }
 }
